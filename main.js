0efd5eb38a860e300488a83e3b73b4858d910462
Rezki Cyprien <cyprien.rezki@hotmail.fr>
2022-07-23 17:48:24 +0200

    add logic product

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: 'Socks',
+      brand : 'Vue Mastery',
       description: 'amazing product',
-      image: './assets/images/socks_green.jpg',
       url: 'https://www.vuemastery.com/',
-      inventory: 0,
-      onSale: false,
-      inStock: false,
+      // inventory: 50,
+      selectedVariant: 0,
+      onSale: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
       sizes: ['S','M','L', 'XL'],
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50  },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
       ]
     }
   },
@@ -23,13 +23,30 @@ const app = Vue.createApp({
     addToCart() {
       this.cart += 1
     },
-    updateImage(variantImage) {
-      this.image = variantImage
+    updateVariant(index) {
+      this.selectedVariant = index
     },
     removeToCart() {
       if(this.cart > 0){
       this.cart -= 1}
     }
+  },
+  computed: {
+    title(){
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].image
+  },
+    inStock() {
+        return this.variants[this.selectedVariant].quantity
+    },
+    saleMessage() {
+      if (this.onSale) {
+          return this.brand + ' ' + this.product + ' is on sale.'
+      }
+      return ''
+  }
   }
 
 
