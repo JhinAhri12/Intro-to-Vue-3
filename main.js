b11f8956006ad0807680ce0121cc53de8a29a897
Rezki Cyprien <cyprien.rezki@hotmail.fr>
2022-07-23 16:32:05 +0200

    add sales information

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const app = Vue.createApp({
       product: 'Socks',
       description: 'amazing product',
       image: './assets/images/socks_green.jpg',
-      url: 'https://www.vuemastery.com/'
+      url: 'https://www.vuemastery.com/',
+      inventory: 8,
+      onSale: true
     }
   }
 
