923051abfca860aa1a3b87be1811e32fc3d9930f
Rezki Cyprien <cyprien.rezki@hotmail.fr>
2022-07-23 17:21:04 +0200

    design logic

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ const app = Vue.createApp({
       description: 'amazing product',
       image: './assets/images/socks_green.jpg',
       url: 'https://www.vuemastery.com/',
-      inventory: 8,
-      onSale: true,
+      inventory: 0,
+      onSale: false,
+      inStock: false,
       details: ['50% cotton', '30% wool', '20% polyester'],
       sizes: ['S','M','L', 'XL'],
       variants: [
